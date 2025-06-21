510b54b95bfcf51aa1afc8746ed0531aae9b1789
Soheila-Amiri <soli.amiri0601@gmail.com>
2025-06-21 15:43:08 +0200

    Collection icon added to the toolbar and can access the collection page by clicking on it

---
+++ app/src/main/java/com/example/hci_test/CollectionPage.java
@@ -0,0 +1,13 @@
+package com.example.hci_test;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CollectionPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_collection);
+    }
+}
